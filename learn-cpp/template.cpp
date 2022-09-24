3388cfe0d03daf770beca385e76dfd8707aa3973
ericlovesmath <ericlovesmath@gmail.com>
2022-09-24 16:48:27 -0700

    Update Template

--- learn-cpp/template.cpp
+++ learn-cpp/template.cpp
@@ -1,9 +1,12 @@
 // Headers {{{
 #include <algorithm>
+#include <cassert>
+#include <cstring>
 #include <iostream>
 #include <iterator>
 #include <map>
 #include <math.h>
+#include <numeric>
 #include <queue>
 #include <set>
 #include <string>
@@ -11,31 +14,61 @@
 #include <vector>
 
 using namespace std;
-#define endl '\n'
 
 void setIO(string name = "") {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
     if (name.size()) {
         freopen((name + ".in").c_str(), "r", stdin);
         freopen((name + ".out").c_str(), "w", stdout);
     }
 }
+
+// Types
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef string str;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+// Vectors
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define sortVec(x) sort(all(x))
+#define pb push_back
+
+// Loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define foreach(a, x) for (auto &a : x)
+
 // }}}
 
 // Problem Statement {{{
 
-// URL
-
-// PROBLEM
-
-// Example
-// In:
-// Out:
-
 // }}}
 
+void solve() {
+    int N;
+    cin >> N;
+}
+
+int T;
+
 int main() {
-    setIO();
+    setIO("test");
+
+    cin >> T;
+    FOR(i, 1, T + 1) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+
+    return 0;
 }
