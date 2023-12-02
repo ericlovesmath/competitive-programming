4ae2bfa9ec34adc34034118bfafeb82ec571e861
ericlovesmath <ericlovesmath@gmail.com>
2023-12-01 23:42:26 -0800

    Clean up AoC 2023 Day 1

--- aoc-2023/day01.py
+++ aoc-2023/day01.py
@@ -1,48 +1,36 @@
 with open("day01.in") as f:
-    inp = f.read().strip()
+    inp = f.read().strip().split("\n")
+
+ans = 0
+for row in inp:
+    digits = [int(c) for c in row if c.isdigit()]
+    ans += digits[0] * 10 + digits[-1]
+print(ans)
 
 words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 words = {words[i]: i + 1 for i in range(9)}
 
 ans = 0
-for row in inp.split():
-    i = 0
-    digits = []
-    while i < len(row):
-        if row[i].isdigit():
-            digits.append(int(row[i]))
-        i += 1
-
-    ans += digits[0] * 10 + digits[-1]
-print(ans)
-
-
-ans = 0
-for row in inp.split():
-    i = 0
-
+for row in inp:
     tens = 0
-    while i < len(row) and tens == 0:
+    for i in range(len(row)):
+        if tens != 0:
+            break
         if row[i].isdigit():
             tens += int(row[i])
-        else:
-            for w in words:
-                if row[i:].startswith(w):
-                    tens += words[w]
-                    break
-        i += 1
+        for w in words:
+            if row[i:].startswith(w):
+                tens += words[w]
 
-    i = len(row) - 1
     ones = 0
-    while i >= 0 and ones == 0:
+    for i in range(len(row) - 1, -1, -1):
+        if ones != 0:
+            break
         if row[i].isdigit():
             ones += int(row[i])
-        else:
-            for w in words:
-                if row[:i + 1].endswith(w):
-                    ones += words[w]
-                    break
-        i -= 1
+        for w in words:
+            if row[:i + 1].endswith(w):
+                ones += words[w]
 
     ans += tens * 10 + ones
 print(ans)
