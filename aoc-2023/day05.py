1e9ef23819c963beaa95693a38c01750a57e3a73
ericlovesmath <ericlovesmath@gmail.com>
2023-12-04 22:01:41 -0800

    Minor Cleanup (:P)

--- aoc-2023/day05.py
+++ aoc-2023/day05.py
@@ -1,15 +1,17 @@
 with open("day05.in") as f:
     start, *inp = f.read().strip().split("\n\n")
+    start = [int(num) for num in start.split()[1:]]
+    N = len(start)
 
 # Problem 1 (RANK 31)
 
-curr = [int(num) for num in start.split()[1:]]
+curr = start.copy()
 
 for mapping in inp:
-    visited = [False for _ in range(len(curr))]
+    visited = [False for _ in range(N)]
     for m in mapping.splitlines()[1:]:
         dst, src, rng = [int(num) for num in m.split()]
-        for i in range(len(curr)):
+        for i in range(N):
             if not visited[i] and src <= curr[i] < src + rng:
                 curr[i] += dst - src
                 visited[i] = True
@@ -18,23 +20,21 @@ print(min(curr))
 
 # Problem 2 (RANK 130, if only I didn't screw up hard...)
 
-curr = [int(num) for num in start.split()[1:]]
-curr = set((curr[i], curr[i] + curr[i + 1] - 1) for i in range(0, len(curr), 2))
-    
+curr = set((start[i], start[i] + start[i + 1] - 1) for i in range(0, N, 2))
+
 for mapping in inp:
     found = set()
     for m in mapping.splitlines()[1:]:
         dst, src, rng = [int(num) for num in m.split()]
-        jmp, src = dst - src, (src, src + rng - 1)
-        for (curr_lo, curr_hi) in list(curr):
-            lo, hi = max(src[0], curr_lo), min(src[1], curr_hi)
-            if lo <= hi:
-                curr.remove((curr_lo, curr_hi))
-                found.add((lo + jmp, hi + jmp))
-                if curr_lo < lo:
-                    curr.add((curr_lo, lo - 1))
-                if hi < curr_hi:
-                    curr.add((hi + 1, curr_hi))
+        for (lo, hi) in list(curr):
+            map_lo, map_hi = max(src, lo), min(src + rng - 1, hi)
+            if map_lo <= map_hi:
+                curr.remove((lo, hi))
+                found.add((map_lo + dst - src, map_hi + dst - src))
+                if lo < map_lo:
+                    curr.add((lo, map_lo - 1))
+                if map_hi < hi:
+                    curr.add((map_hi + 1, hi))
     curr = curr.union(found)
 
 print(min(curr)[0])
