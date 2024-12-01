c7336232782ee1aa4b627187764a03d935dc4222
ericlovesmath <ericlovesmath@gmail.com>
2024-11-30 16:18:57 -0800

    AoC 2024 prep

---
+++ aoc-2024/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Advent of Code 2024";
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { nixpkgs-unstable, ... }:
+    let
+      system = "aarch64-darwin";
+    in
+    {
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs-unstable { inherit system; };
+        in
+        pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (pkgs: with pkgs; [
+              pandas
+              numpy
+            ]))
+          ];
+        };
+    };
+}
